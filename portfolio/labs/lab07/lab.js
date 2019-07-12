d13222b4cfaa1e6e7bfc3591a8533e0fcad833e5
y204682022 <52515289+y204682022@users.noreply.github.com>
2019-07-12 17:35:16 +0800

    Add files via upload

--- portfolio/labs/lab07/lab.js
+++ portfolio/labs/lab07/lab.js
@@ -1,6 +1,29 @@
 /*
 
 */
+/*function start(e){
+    $(document).mousemove(function(event){
+        var p = $("#svgimage2");
+
+        var position = p.position();
+
+        console.log('position = ' + position.left + ', ' + position.top);
+
+        var myX = event.pageX - Math.round(position.left);
+        var myY = event.pageY - Math.round(position.top) +500;
+
+        $("#s5").html("<div style='position:absolute; border-style:none; TOP:"
+            + event.pageY + "px; LEFT:"
+            + event.pageX + "px;'>" + "&nbsp&nbsp&nbsp&nbsp("
+            + myX + ", "
+            + myY + ")"
+            + "</div>")
+    });
+
+
+}*/
+
+
 
 function analyze() {
     var m, n;
@@ -23,25 +46,54 @@ function analyze() {
         return;
       }
 
+      m = remain.search('d=');
+      console.log('d= m = ' +m);
+      remain = remain.slice(m);
+
       m = remain.search(/m/i);
-      console.log('m = ' + m);
+      console.log('m  m =  ' + m);
 
-      n = remain.search('z"'); // /z/i
-      console.log('n = ' + n);
-
+      n = remain.search(/z/i); // /z/i
+      console.log('z  n = ' + n);
       subs = remain.slice(m, n+1); // z 也要包含
       console.log('subs = ' + subs);
 
+      var subs2 = subs.replace('440', '480');
+      var subs2 = subs2.replace('462', '262');
+
+
       var path = image.path(subs).fill('none').stroke({ color:'crimson' , width: 5 }).draggable();
 
-      remain = remain.slice(n+1); // z 也要移除
+      path.plot(subs2).draggable();
+
+      //remain = remain.slice(n+1); // z 也要移除
       //console.log('remain = ' + remain);
 
       //var pathString = "M382 371C440 281 80 162 82 314 84 467 324 462 382 371z"
-      //var newPath = Snap.path.toCubic(pathString);
+      var newPath = Snap.path.toCubic(subs);
+      
+      console.log(' newPath.length = ' + newPath.length);
+      newPath.forEach(function(element){
+        console.log(element);
 
-      var newPath = [];
-      var move = '';
+
+      });
+    for ( var i = 0 ; i < (newPath.length - 1);i++ ){
+        for(var j = 0; j < newPath[i].length ;j++){
+            console.log(' newPath[' + i + '][j] =  '+ newPath[i][j] );
+        }
+        if( i==0 ){
+            var circle = image.circle(20).fill('red').stroke('blue').move(newPath[i][1]-10, newPath[i][2]-10).draggable();
+        }else{
+            var circle = image.circle(10).fill('pink').stroke('blue').move(newPath[i][1]-5, newPath[i][2]-5).draggable();
+            var circle = image.circle(10).fill('pink').stroke('blue').move(newPath[i][3]-5, newPath[i][4]-5).draggable();
+            var circle = image.circle(10).fill('pink').stroke('blue').move(newPath[i][5]-5, newPath[i][6]-5).draggable();
+        }
+    }
+
+      //var newPath = [];
+      //var move = '';
+
 
       m = subs.search(/c/i);
       console.log('m = subs.search(/c/i);');
@@ -51,11 +103,14 @@ function analyze() {
       console.log('n = subs.search(/z/i);');
       console.log('n = ' + n);
 
-
+      var move='';
       move = subs.slice(1, m);
       console.log('move.length = ' + move.length);
       console.log('move = ' + move);
 
+      move = move.replace(',', ' ');
+      console.log('move.length = '+ move.length);
+      console.log('move = '+ move);
       // https://stackoverflow.com/questions/40282519/split-string-by-multiple-spaces-nodejs
       var temp;
       temp = move.trim().split(/\s+/);
@@ -74,6 +129,7 @@ function analyze() {
 
       var circle = image.circle(20).fill('red').stroke('blue').move(x-10, y-10).draggable();
 
+      var newPath = [];
       /*
       temp.forEach(function(element) {
         newPath.push(element);
@@ -174,6 +230,23 @@ function handleDragOver(evt) {
 }
 
 function start(e) {
+     $(document).mousemove(function(event){
+        var p = $("#svgimage2");
+
+        var position = p.position();
+
+        console.log('position = ' + position.left + ', ' + position.top);
+
+        var myX = event.pageX - Math.round(position.left);
+        var myY = event.pageY - Math.round(position.top) +500;
+
+        $("#s5").html("<div style='position:absolute; border-style:none; TOP:"
+            + event.pageY + "px; LEFT:"
+            + event.pageX + "px;'>" + "&nbsp&nbsp&nbsp&nbsp("
+            + myX + ", "
+            + myY + ")"
+            + "</div>")
+    });
 
   document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
